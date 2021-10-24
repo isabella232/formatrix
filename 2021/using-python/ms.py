91702d8cc7cf6404b121ae2755bcd2c998f27b53
healingdrawing <healingdrawing@gmail.com>
2021-10-24 12:40:31 +0300

    added checking for no pi inside determinant case -> use determinant as solution

--- 2021/using-python/ms.py
+++ 2021/using-python/ms.py
@@ -7,6 +7,14 @@ class MS:
     def __init__(self) -> None:
         pass
 
+    def pi_in_matrixDeterminant(self, d) -> bool:
+        if "pi" in str(d):
+            return True
+        return False
+
     def solve_pi(self, m) -> list:
         mdet = m.det()
-        return s.solve(mdet, s.pi, check=False, rational=None)
+        if self.pi_in_matrixDeterminant(mdet):
+            return s.solve(mdet, s.pi, check=False, rational=None)
+        # if no pi inside det then return det as solution, packed in list
+        return [mdet]
